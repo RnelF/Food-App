ac9cdc77dc63f79ea3a61f77ffcd1b95cc89e926
Arnel Francia <nelffff@gmail.com>
2024-07-30 19:22:00 +0800

    updated

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import styles from "./search.module.css";
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "b000760182f248f1b5b79180ce34edb2";
 
@@ -16,8 +16,9 @@ export default function Search({ foodData, setFoodData }) {
   }, [query]);
 
   return (
-    <div>
+    <div className={styles.searchContainer}>
       <input
+        className={styles.input}
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         type="text"
